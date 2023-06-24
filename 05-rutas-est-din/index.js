be88a01fa391dd9642e13634d356384e5731ae37
Gadd88 <matiassaade@gmail.com>
2023-06-24 03:12:44 -0300

    ejercicitando con query params y url params

--- 05-rutas-est-din/index.js
+++ 05-rutas-est-din/index.js
@@ -11,9 +11,9 @@ const app = express();
 
 //array de ejemplo
 const cursos = [
-    {id: 1, nombre: 'Javascript', categoria: 'Programación'},
-    {id: 2, nombre: 'React JS', categoria: 'Programación'},
-    {id: 3, nombre: 'Vue JS', categoria: 'Programación'},
+    {id: 1, nombre: 'Javascript', categoria: 'Programacion'},
+    {id: 2, nombre: 'React JS', categoria: 'Programacion'},
+    {id: 3, nombre: 'Vue JS', categoria: 'Programacion'},
     {id: 4, nombre: 'SQL', categoria: 'Datos'},
     {id: 5, nombre: 'MongoDB', categoria: 'Datos'},
     {id: 6, nombre: 'Ecommerce', categoria: 'Producto'},
@@ -38,28 +38,33 @@ app.get('/cursos', (req,res) => {
     req.query.nombre
     req.query.categoria
 
-        //validamos que el objeto req.query tiene propiedades internas
-    const queryParams = Object.keys(req.query);
+    //validamos que el objeto req.query tiene propiedades internas
+    const { nombre, categoria } = req.query; //destructuring para ocupar los parametros que se requieran
+    const queryParams = Object.keys(req.query); //generamos un array con las llaves del objeto req.query
+    let resultado = [];
+
     if (queryParams.length === 0){
         console.log('No llegan parámetros. Envío el set de datos');
-        res.send('Llegan parámetros');
+        res.status(200).render(path.join(views, 'cursos'), {cursos});
     }else {
         console.log('Llegan los parámetros nombre y categoria');
-    }
-
-    for (let curso of cursos){
-        if(curso.nombre.toLowerCase().includes(req.query.nombre.toLowerCase())
-        && curso.categoria.toLowerCase().includes(req.query.categoria.toLowerCase())){
-            resultado.push(curso)
+        for (let curso of cursos){
+            if(curso.nombre.toLowerCase().includes(nombre.toLowerCase())
+            && curso.categoria.toLowerCase().includes(categoria.toLowerCase())){
+                resultado.push(curso)
+            }
         }
+        resultado.length > 0 ? 
+                    res.json(resultado) : 
+                    res.json([{
+                        id: 'Error', 
+                        descripcion: 'No se encontraron coincidencias.'}]);
     }
-
-
-    res.render(path.join(views, 'cursos'));
 });
 
 //URL PARAMS ***********************************************************
 app.get('/cursos/:categoria', (req, res) =>{
+
     //aplicamos trim() para eliminar los espacios en blanco del parametro recibido y toLowerCase() para evitar errores por las mayusculas que podrian emplearse.
     let parametro = req.params.categoria.trim().toLowerCase();
     
@@ -79,16 +84,71 @@ app.get('/cursos/:categoria', (req, res) =>{
                         descripcion: 'No se encontraron coincidencias.'}]);
 });
 
+app.get('/curso/codigo/:id', (req,res) =>{
+    const { id } = req.query
+    let resultado = [];
+    let idBuscado = parseInt(id);
+    for (let curso of cursos){
+        if (idBuscado === parseInt(curso.id)){
+            console.log(idBuscado)
+        }
+    }
+})
 
+// ***************************************************************************************
+// ***************************************************************************************EJEMPLO
+// ***************************************************************************************
+// ***************************************************************************************
 
+const coches = [
+    {id:1 ,marca: 'Ford', anio: 2017, color: 'rojo'},
+    {id:2 ,marca: 'Fiat', anio: 2019, color: 'blanco'},
+    {id:3 ,marca: 'Chevrolet', anio: 2020, color: 'verde'},
+    {id:4 ,marca: 'Honda', anio: 2023, color: 'gris'},
+    {id:5 ,marca: 'Chery', anio: 2011, color: 'azul'},
+];
 
-app.get('*', (req,res) => {
-    res.status('404').send(`{
-        "error": "404", 
-        "description": "No se encuentra la ruta especificada."
-    }`);
+app.get('/coches', (req,res) => {
+    const { id, anio, color } = req.query;
+
+    const coche = coches.find( elemento => elemento.id === Number(id));
+
+    
+    if (!coche) { //CONDICION DE ESCAPE, por si no se encuentra el ID
+        res.status(400).send(`<h1>Error</h1>
+        <p>ID incorrecto</p>`)
+    }
+    
+    res.status(200).send(coche);
 });
 
+app.get('/coches/id/:id/marca/:marca/anio/:anio', (req,res) => {
+    const { id, anio, marca } = req.params;
+
+    const coche = coches.find( elemento => elemento.id === Number(id));
+    coche.anio = anio;
+    coche.marca = marca;
+
+    
+    if (!coche) { //CONDICION DE ESCAPE, por si no se encuentra el ID
+        res.status(400).send(`<h1>Error</h1>
+        <p>ID incorrecto</p>`)
+    }
+    
+    res.status(200).send(coche);
+});
+
+// ***************************************************************************************
+// ***************************************************************************************
+// ***************************************************************************************
+// ***************************************************************************************
+
+app.get('*', (req,res) => {
+    res.status(404).send(`{
+        <h1>Error</h1>
+        <p>No se encuentra el recurso solicitado</p>
+    }`);
+});
 
 
 app.listen(PORT, ()=>{

b02a904eef6511dc2a0b2071eda67a2b53ad121a
Gadd88 <matiassaade@gmail.com>
2023-06-27 02:02:03 -0300

    agregados los metodos y funciones

---
+++ 09-proyecto-api-rest/src/assets/data.manager.js
@@ -0,0 +1,104 @@
+const fs = require('fs');
+const path = require('path')
+
+const rutaProductos = path.join(__dirname, '../public/lista-productos.json');
+
+function cargarDatos(contenido) {
+    return new Promise((resolve, reject) => {
+        fs.writeFile(rutaProductos, JSON.stringify(contenido, null, "\t"), "utf8", (error) => {
+            if (error) reject(new Error("Error. No se puede escribir"));
+
+            resolve(true);
+        });
+    });
+};
+
+
+function leerDatos(){
+    return new Promise((resolve,reject) =>{
+        fs.readFile(rutaProductos, 'utf-8', (error, resultado) =>{
+            if(!error){
+                console.log(`Archivo ${rutaProductos} cargado correctamente`)
+                resolve(JSON.parse(resultado));
+            }else{
+                reject(new Error('Error. No se pudo leer el archivo'));
+            }
+        });
+    })
+};
+
+function generarId(productos){
+    let mayorId = 0;
+
+    productos.forEach((producto) => {
+        if (Number(producto.id) > mayorId){
+            mayorId = Number(producto.id)
+        }
+    })
+
+    return mayorId + 1;
+}
+
+
+async function listaProductos(){
+    const productos = await leerDatos();
+    return productos
+}
+
+async function borrarProducto(id){
+    if(!id) throw new Error("Error con el ID. No existe el producto")
+    
+    const productos = await leerDatos();
+
+    const index = productos.findIndex((elemento) => elemento.id === id)
+
+    if(index < 0) throw new Error("Error. El ID solicitado no corresponde a un producto en la BBDD")
+
+    const producto = productos[index];
+    productos.splice(index, 1)
+    await cargarDatos(producto)
+    
+    return producto;   
+    
+}
+
+async function nuevoProducto(producto){
+    if (!producto?.id || !producto?.nombre || !producto?.marca || !producto?.categoria || !producto?.precio || !producto?.stock) throw new Error('Error. Datos Incompletos')
+
+    const productos = await leerDatos();
+    const nuevoProd = {id: generarId(productos), ...producto}
+
+    productos.push(nuevoProd);
+    await cargarDatos(productos)
+
+    return nuevoProd;
+}
+
+async function actualizarProducto(producto){
+    if (!producto?.id || !producto?.nombre || !producto?.marca || !producto?.categoria || !producto?.precio || !producto?.stock) throw new Error('Error. Datos Incompletos')
+
+    const productos = await leerDatos();
+    const index = productos.findIndex((elemento)=> elemento.id === producto.id)
+
+    if (index<0) throw new Error('Error. El ID no corresponde con ningun producto de la lista');
+
+    productos[index] = producto;
+    await cargarDatos(productos);
+
+    return productos(index);
+}
+
+async function idProducto(id){
+    const productos = await leerDatos();
+
+    let producto = productos.find(elemento => (elemento.id === id))
+
+    if(!producto){
+        throw new Error('No se encuentra el producto con el ID solicitado')
+    }else{
+        return producto
+    }
+}
+
+
+module.exports = { borrarProducto, nuevoProducto, actualizarProducto, idProducto, listaProductos }
---
+++ 09-proyecto-api-rest/src/server.js
@@ -0,0 +1,69 @@
+const express = require('express');
+const path = require('path');
+const fs = require('fs');
+require('dotenv').config();
+const { idProducto, listaProductos, borrarProducto, actualizarProducto } = require('./assets/data.manager');
+
+// Iniciando server
+const server = express();
+
+// Rutas ********************************************************************************
+server.get('/', (req,res)=>{
+    res.status(200).send("<h1>Proyecto Api Rest - Backend NodeJs</h1>")
+})
+
+
+//Lista General de Productos ************************************************************
+server.get('/productos', (req,res)=>{
+
+    listaProductos()
+        .then((productos)=> res.status(200).send(productos))
+        .catch((error)=> res.status(404).send(error.message))
+
+})
+
+//Obtener producto por ID ***************************************************************
+server.get('/productos/:id', (req,res)=>{
+    const id = Number(req.params.id);
+
+    idProducto(id)
+        .then((producto)=> res.status(200).send(producto))
+        .catch((error)=> res.status(404).send(error.message))
+})
+
+//Crear un nuevo producto ***************************************************************
+server.post('/productos', (req,res) => {
+    const { nombre, marca, categoria, precio, stock } = req.body;
+
+    nuevoProducto({ nombre, marca, categoria, precio, stock })
+        .then((producto) => res.status(201).send(producto))
+        .catch((error)=> res.status(400).send(error.message))
+})
+
+//Actualizar un producto existente ******************************************************
+server.put('/productos/:id', (req,res)=>{
+    const { id } = req.params;
+    const { nombre, marca, categoria, precio, stock } = req.body;
+
+    actualizarProducto({id: Number(id), nombre, marca, categoria, precio, stock})
+        .then((producto) => res.status(200).send(producto))
+        .catch((error)=> res.status(400).send(error.message))
+})
+
+//Eliminar un producto existente ********************************************************
+server.delete('/productos/:id', (req,res)=>{
+    const { id } = req.params;
+
+    borrarProducto(Number(id))
+        .then((producto) => res.status(200).send(producto))
+        .catch((error)=> res.status(400).send(error.message))
+})
+
+//************************************************************
+server.get('*', (req,res)=>{
+    res.status(404).send("<h1>Pagina no encontrada</h1>")
+})
+
+server.listen(process.env.SERVER_PORT, process.env.SERVER_HOST, () => {
+    console.log(`Servidor corriendo en: http://${process.env.SERVER_HOST}:${process.env.SERVER_PORT}`)
+})

61f600e598984cb27d7f357dd89b5536d5dc4225
Gadd88 <matiassaade@gmail.com>
2023-07-01 21:55:25 -0300

    git

--- 10-mongodb/src/server.js
+++ 10-mongodb/src/server.js
@@ -3,7 +3,7 @@ require('dotenv').config();
 const server = express();
 const HOST = process.env.HOST || '127.0.0.1';
 const PORT = process.env.PORT || 3000;
-const URI = process.env.URI;
+const URI = `mongodb+srv://${process.env.CLIENT}:${process.env.DB_PASS}@db-argprog.4jbosth.mongodb.net/`;
 const client = process.env.CLIENT;
 const pass = process.env.PASS;
 const cluster = process.env.CLUSTER;
